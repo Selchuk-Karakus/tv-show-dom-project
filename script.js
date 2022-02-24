db30791a0576647d57ef10dede726bc0ea834dc3
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-02-24 14:41:17 +0000

    Add level 200 and style

--- script.js
+++ script.js
@@ -14,10 +14,13 @@ footerEl.append(paragraphEl);
 paragraphEl.appendChild(footerLinkEl);
 document.body.appendChild(footerEl);
 
-function setup() {
-  const allEpisodes = getAllEpisodes();
+let allEpisodes = [];
+const allEpisodesCounter = getAllEpisodes().length;
+
+const setup = () => {
+  allEpisodes = getAllEpisodes();
   getEpisodeOfTvShows(allEpisodes);
-}
+};
 
 // Render each episode to the website //
 const renderEpisodeToWebsite = (name, season, number, image, summary) => {
@@ -32,6 +35,8 @@ const renderEpisodeToWebsite = (name, season, number, image, summary) => {
 
 // Create all episodes of tv shows //
 const getEpisodeOfTvShows = (episodeList) => {
+  rootElem.innerHTML = "";
+
   episodeList.forEach(({ name, season, number, image, summary }) => {
     divCardEl = document.createElement("div");
     divCardEl.classList.add("card");
@@ -52,11 +57,46 @@ const getEpisodeOfTvShows = (episodeList) => {
   // Add and wait for action to remove the watched episode by clicking on corresponding image //
   let imageEl = document.getElementsByClassName("card-img-top");
   Array.from(imageEl).forEach((image) => {
-    image.addEventListener("click", (e) => {
-      let removeTarget = e.target.parentNode;
-      removeTarget.parentNode.removeChild(removeTarget);
-    }, false);
+    image.addEventListener(
+      "click",
+      (e) => {
+        let removeTarget = e.target.parentNode;
+        removeTarget.parentNode.removeChild(removeTarget);
+      },
+      false
+    );
   });
 };
+
+// Create search bar component //
+const divSearchEl = document.createElement("div");
+divSearchEl.classList.add("search-bar-root");
+const inputSearchEl = document.createElement("input");
+inputSearchEl.classList.add("search-query");
+inputSearchEl.setAttribute("type", "text");
+inputSearchEl.setAttribute("placeholder", "Search for any episode");
+const paragraphCounterEl = document.createElement("p");
+paragraphCounterEl.innerHTML = `Displaying ${allEpisodesCounter} / 0 episodes`;
+divSearchEl.appendChild(inputSearchEl);
+divSearchEl.appendChild(paragraphCounterEl);
+document.body.prepend(divSearchEl);
+
+// Filter out episodes matching search term within episode title or episode summary
+const handleSearchQuery = (e) => {
+  const searchQuery = e.target.value.toLowerCase();
+  let filteredEpisodes = allEpisodes.filter(({ name, summary }) => {
+    return (
+      name.toLocaleLowerCase().includes(searchQuery) ||
+      summary.toLocaleLowerCase().includes(searchQuery)
+    );
+  });
+  getEpisodeOfTvShows(filteredEpisodes);
+
+  const filteredEpisodesCounter = filteredEpisodes.length;
+  const para = `${allEpisodesCounter} / ${filteredEpisodesCounter}`;
+  paragraphCounterEl.innerHTML = `Displaying ${para} episodes`;
+};
+
+inputSearchEl.addEventListener("keyup", handleSearchQuery);
 
 window.onload = setup;
