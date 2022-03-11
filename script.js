89130f1db6022902913cb5344967a025b18b3e41
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-11 12:47:55 +0000

    Add level 400, tidy up sytyle

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ footerEl.append(paragraphEl);
 paragraphEl.appendChild(footerLinkEl);
 document.body.appendChild(footerEl);
 
-let allEpisodes = getAllEpisodes();
-const allEpisodesCounter = getAllEpisodes().length;
+let allEpisodes;
+let allEpisodesCounter;
+let allShows = getAllShows();
 
 const setup = () => {
-  createRequest(82).then((data) => {
+  let renderDefaultTvShow = 167;
+  createRequest(renderDefaultTvShow).then((data) => {
     getEpisodeOfTvShows(data);
+    createEpisodeSelectOption(data);
+    allEpisodes = data;
+    allEpisodesCounter = data.length;
+    paragraphCounterEl.innerHTML = `Displaying ${allEpisodesCounter} / ${allEpisodesCounter} episodes`;
   });
 };
 
@@ -101,7 +107,6 @@ inputSearchEl.classList.add("search-query");
 inputSearchEl.setAttribute("type", "text");
 inputSearchEl.setAttribute("placeholder", "Search for any episode");
 const paragraphCounterEl = document.createElement("p");
-paragraphCounterEl.innerHTML = `Displaying ${allEpisodesCounter} / ${allEpisodesCounter} episodes`;
 divSearchEl.appendChild(inputSearchEl);
 divSearchEl.appendChild(paragraphCounterEl);
 document.body.prepend(divSearchEl);
@@ -111,8 +116,8 @@ const handleSearchQuery = (e) => {
   const searchQuery = e.target.value.toLowerCase();
   let filteredEpisodes = allEpisodes.filter(({ name, summary }) => {
     return (
-      name.toLocaleLowerCase().includes(searchQuery) ||
-      summary.toLocaleLowerCase().includes(searchQuery)
+      name.toLowerCase().includes(searchQuery) ||
+      summary.toLowerCase().includes(searchQuery)
     );
   });
   getEpisodeOfTvShows(filteredEpisodes);
@@ -124,28 +129,32 @@ const handleSearchQuery = (e) => {
 
 inputSearchEl.addEventListener("keyup", handleSearchQuery);
 
-// Create select input component //
+// Create select input component for Episodes //
 const inputSelectEl = document.createElement("select");
 inputSelectEl.classList.add("select-query");
 inputSelectEl.setAttribute("id", "option");
 inputSelectEl.setAttribute("type", "select");
 divSearchEl.prepend(inputSelectEl);
 
-allEpisodes.forEach((episode) => {
-  const optionEl = document.createElement("option");
-  optionEl.value = episode.id;
-  optionEl.innerText = `S${episode.season
-    .toString()
-    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
-    episode.name
-  }`;
-  inputSelectEl.append(optionEl);
-});
+const createEpisodeSelectOption = (allEpisodes) => {
+  inputSelectEl.innerHTML = "";
+  allEpisodes.forEach((episode) => {
+    const optionEl = document.createElement("option");
+    optionEl.value = episode.id;
+    optionEl.innerText = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+      episode.name
+    }`;
+    inputSelectEl.append(optionEl);
+  });
+};
 
 // Check to see if selected episode id matches with any in the data if so call the render method with the found episode and render it to the webpage
 const selectedEpisode = document.getElementById("option");
 selectedEpisode.addEventListener("change", () => {
   rootElem.innerHTML = "";
+  console.log(selectedEpisode);
   allEpisodes.find((episode) => {
     if (selectedEpisode.value == episode.id) {
       const { name, season, number, image, summary } = episode;
@@ -163,6 +172,38 @@ divSearchEl.appendChild(buttonBackEl);
 buttonBackEl.addEventListener("click", () => {
   setup();
   paragraphCounterEl.innerHTML = `Displaying ${allEpisodesCounter} / ${allEpisodesCounter} episodes`;
+});
+
+// Create select input component for Shows//
+const inputShowSelectEl = document.createElement("select");
+inputShowSelectEl.classList.add("select-query", "select-query-show");
+inputShowSelectEl.setAttribute("id", "option-show");
+inputShowSelectEl.setAttribute("type", "select");
+divSearchEl.prepend(inputShowSelectEl);
+
+const sortShowByName = (a, b) => {
+  return a.name > b.name ? 1 : -1;
+};
+
+let showSortedAtoZ = allShows.sort(sortShowByName);
+
+showSortedAtoZ.forEach(({ id, name }) => {
+  const optionEl = document.createElement("option");
+  optionEl.value = id;
+  optionEl.innerText = `${name}`;
+  inputShowSelectEl.append(optionEl);
+});
+
+inputShowSelectEl.addEventListener("change", () => {
+  let showId = inputShowSelectEl.value;
+  createRequest(showId).then((data) => {
+    getEpisodeOfTvShows(data);
+    createEpisodeSelectOption(data);
+    allEpisodes = data;
+    allEpisodesCounter = allEpisodes.length;
+    console.log(allEpisodesCounter);
+    paragraphCounterEl.innerHTML = `Displaying ${allEpisodesCounter} / ${allEpisodesCounter} episodes`;
+  });
 });
 
 window.onload = setup;
